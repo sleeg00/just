504a5f42011f35df3b4b548748cdfc1d5b7af54e
sleeg00 <slee000220@gmail.com>
2023-03-25 15:50:40 +0900

    application.yml 수정

--- src/main/java/com/example/just/Config/SecurityConfig.java
+++ src/main/java/com/example/just/Config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {  //해당 URL은 필터 거치지 않겠다
         return (web -> web.ignoring().antMatchers("/api/**"));
+
         //return (web -> web.ignoring().antMatchers("/test"));
     }
 
