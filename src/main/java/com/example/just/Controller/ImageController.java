76f2be2eda956aaf9e6ffee60b440239f69f5274
sleeg00 <slee000220@gmail.com>
2023-03-25 21:42:12 +0900

    application.yml 수정

--- src/main/java/com/example/just/Controller/ImageController.java
+++ src/main/java/com/example/just/Controller/ImageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/image")
+@RequestMapping("/api/image")
 public class ImageController {
     @Autowired
     private ImageService imageService;
