2d7ffd6a05ab6c67ba37cad793deb568f2c6cc61
sleeg00 <slee000220@gmail.com>
2023-03-26 21:05:54 +0900

    gradle.yml 수정 && DB연결

--- src/test/java/com/example/just/JustApplicationTest.java
+++ src/test/java/com/example/just/JustApplicationTest.java
@@ -11,19 +11,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class JustApplicationTest {
 
 
-    private int port=8080;
-
+    private int port=8081;
+    static{
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
     @Test
     public void testServerIsRunning() throws Exception {
-        String url = "http://localhost:" + port + "/";
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-        int statusCode = connection.getResponseCode();
-        assertThat(statusCode).isEqualTo(200);
+
+
+        System.out.println("!");
     }
 }
