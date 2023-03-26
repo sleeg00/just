1222a1b9ca16637f918889d2dbd43d3315a711dc
sleeg00 <slee000220@gmail.com>
2023-03-26 16:26:15 +0900

    서버 CI/CD 성공 Test

--- src/main/generated/com/example/just/Mapper/PostMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/PostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-26T15:56:53+0900",
+    date = "2023-03-26T16:17:57+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
