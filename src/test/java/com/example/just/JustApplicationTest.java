e071d29019bc5f5586f303116a12e47c8b5f90e2
sleeg00 <slee000220@gmail.com>
2023-03-28 15:15:06 +0900

    github Action수정 test가능하도록

--- src/test/java/com/example/just/JustApplicationTest.java
+++ src/test/java/com/example/just/JustApplicationTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JustApplicationTest {
 
