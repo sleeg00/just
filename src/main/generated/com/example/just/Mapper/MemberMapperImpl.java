d8014a8dd7fca24852be887ac9096e383f570e0a
sleeg00 <slee000220@gmail.com>
2023-03-28 12:41:42 +0900

    github Action수정 test가능하도록

--- src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-27T19:42:04+0900",
-    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.15.1 (Oracle Corporation)"
+    date = "2023-03-28T12:39:22+0900",
+    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
 public class MemberMapperImpl implements MemberMapper {
