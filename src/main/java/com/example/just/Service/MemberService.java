aa8ee344f85a1a0409311f0ae37f04dd3f72a66a
mjh000526 <mjh000526@gmail.com>
2023-03-28 22:38:23 +0900

    #6 apple 로그인 수정

--- src/main/java/com/example/just/Service/MemberService.java
+++ src/main/java/com/example/just/Service/MemberService.java
@@ -5,10 +5,13 @@ import com.example.just.Dao.Member;
 import com.example.just.Dto.MemberDto;
 import com.example.just.Mapper.MemberMapper;
 import com.example.just.Repository.MemberRepository;
+import com.example.just.jwt.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class MemberService {
@@ -17,6 +20,8 @@ public class MemberService {
     MemberRepository memberRepository;
     @Autowired
     MemberMapper memberMapper;
+    @Autowired
+    JwtProvider jwtProvider;
 
     public ResponseEntity<BasicResponse> join(MemberDto memberDto) {
         Member member = memberMapper.toEntity(memberDto);
@@ -28,5 +33,13 @@ public class MemberService {
                 .result(null)
                 .count(1).build();
         return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
+    }
+
+    public ResponseEntity drop(HttpServletRequest request){
+        String token = request.getHeader("access_token");
+        Long id = Long.valueOf(jwtProvider.getIdFromToken(token)); //토큰으로 id추출
+        String email = jwtProvider.getEmailFromRefreshToken(token);
+        memberRepository.deleteById(id);
+        return new ResponseEntity<>(email + "삭제",HttpStatus.OK);
     }
 }
