8b993d5f4ecb5f3fcba8b682276f233240fd2537
sleeg00 <slee000220@gmail.com>
2023-09-11 14:58:36 +0900

    Server Test

--- src/main/java/com/example/just/Service/ImageService.java
+++ src/main/java/com/example/just/Service/ImageService.java
@@ -1,30 +1,12 @@
 package com.example.just.Service;
 
-import com.amazonaws.HttpMethod;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
+
 import org.springframework.stereotype.Service;
 
-import java.net.URL;
-import java.util.Date;
 
 @Service
 
 public class ImageService {
 
-    @Autowired
-    private AmazonS3 s3Client;
 
-    private String bucketName = "s3-upload-image-just";
-
-    public String getPresignedUrl(String filename) {
-        Date expiration = new Date(System.currentTimeMillis() + 3600000); // 1 hour expiration time
-        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, filename)
-                .withMethod(HttpMethod.GET)
-                .withExpiration(expiration);
-        URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
-        return url.toString();
-    }
 }
