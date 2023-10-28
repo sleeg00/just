4adda81033b8dec866d052d324c3e99b24cfb0d0
sleeg00 <slee000220@gmail.com>
2023-10-28 17:17:48 +0900

    Revert "Revert "Revert "Refector: Post API"""
    This reverts commit c8f2320601c2afbf8a7d244366fb1e1c1104b423.

--- src/main/java/com/example/just/Mapper/PostMapper.java
+++ src/main/java/com/example/just/Mapper/PostMapper.java
@@ -1,6 +1,8 @@
 package com.example.just.Mapper;
 
 import com.example.just.Dao.Post;
+import com.example.just.Dto.PostDto;
+import com.example.just.Dto.PostPostDto;
 import com.example.just.Dto.PutPostDto;
 import org.mapstruct.Mapper;
 
