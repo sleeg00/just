a8880481ea663284a8d6f13a8423cd7c84001cd4
sleeg00 <slee000220@gmail.com>
2023-10-28 15:23:22 +0900

    Fix: login.html수정

--- src/main/generated/com/example/just/Mapper/PostMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/PostMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-11T15:01:33+0900",
+    date = "2023-10-28T15:14:51+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
