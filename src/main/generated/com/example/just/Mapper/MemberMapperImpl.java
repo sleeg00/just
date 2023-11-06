d38d21fef836b952955847dcf93685bf4332a7d3
문준호 <munjunho@munjunhoui-MacBookAir.local>
2023-11-06 12:22:02 +0900

    build error

--- src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-06T12:13:52+0900",
+    date = "2023-11-06T12:19:28+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
