22106a361ff83c8d6bb143fb23f6b941fe707ebf
문준호 <munjunho@munjunhoui-MacBookAir.local>
2023-11-06 18:55:16 +0900

    commentDto modify

--- src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-06T18:15:08+0900",
+    date = "2023-11-06T18:52:41+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
