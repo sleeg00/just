3c7cbf3453426f55a8f419780b584073e1f3aead
문준호 <munjunho@munjunhoui-MacBookAir.local>
2023-11-07 17:35:40 +0900

    my comment list response modify

--- src/main/java/com/example/just/Dto/ResponseGetPostDto.java
+++ src/main/java/com/example/just/Dto/ResponseGetPostDto.java
@@ -2,6 +2,9 @@ package com.example.just.Dto;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.just.Dao.Post;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +28,19 @@ public class ResponseGetPostDto {
 
     private Long post_like_size;
     private int blamed_count;
+
+    public ResponseGetPostDto(){}
+    public ResponseGetPostDto(Post post){
+        post_id = post.getPost_id();
+        post_content = post.getPostContent().stream()
+                .map(conent -> new String(conent))
+                .collect(Collectors.toList());
+        post_tag = post.getPost_tag();
+        post_picture = post.getPost_picture();
+        post_create_time = post.getPost_create_time();
+        secret = post.getSecret();
+        post_category = post.getPost_category();
+        comment_size = (long) post.getComments().size();
+        blamed_count = post.getBlamedCount();
+    }
 }
--- src/main/java/com/example/just/Repository/CommentRepository.java
+++ src/main/java/com/example/just/Repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.example.just.Repository;
 
 import com.example.just.Dao.Comment;
+import com.example.just.Dao.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByBlamedCountGreaterThanEqualOrderByBlamedCountDesc(int blame_count);
+
+    List<Comment> findAllByMember(Member member);
 }
