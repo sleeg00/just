6047b9f614edbee58af9caab964d1d92f05ecbbb
문준호 <munjunho@munjunhoui-MacBookAir.local>
2023-11-07 17:35:40 +0900

    my comment list response modify

--- src/main/generated/com/example/just/Mapper/PostMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/PostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-06T19:32:56+0900",
+    date = "2023-11-07T17:32:49+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
