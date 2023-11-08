3774bdb155a87ed2b973f7ff60c5389063d107c0
sleeg00 <slee000220@gmail.com>
2023-11-08 12:10:34 +0900

    Fix: Comment API

--- src/main/java/com/example/just/Dto/ResponseGetMemberPostDto.java
+++ src/main/java/com/example/just/Dto/ResponseGetMemberPostDto.java
@@ -1,5 +1,8 @@
 package com.example.just.Dto;
 
+import com.example.just.Dao.Member;
+import com.example.just.Dao.Post;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import lombok.Getter;
@@ -26,4 +29,29 @@ public class ResponseGetMemberPostDto {
     private Long post_like_size;
     private int blamed_count;
     private boolean like;
+
+    public ResponseGetMemberPostDto(Post post, Long member_id, Member member) {
+        this.post_id = post.getPost_id();
+        this.post_content = post.getPostContent();
+        this.post_tag = post.getPost_tag();
+        this.post_create_time = post.getPost_create_time();
+        this.secret = post.getSecret();
+        this.post_category = post.getPost_category();
+        this.comment_size = Long.valueOf(post.getComments().size());
+        this.post_like_size = post.getPost_like();
+        this.blamed_count = post.getBlamedCount();
+        this.like = false;
+        for (int i = 0; i < post.getLikedMembers().size(); i++) {
+            System.out.println(post.getLikedMembers().get(i).getId());
+            if (post.getLikedMembers().get(i).getId() == member_id) {
+
+                this.like = true;
+                break;
+            }
+        }
+    }
+
+    public ResponseGetMemberPostDto() {
+
+    }
 }
