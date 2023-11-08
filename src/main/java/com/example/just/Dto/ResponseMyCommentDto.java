f95b12b41d9a7d25fff8d2026a838257c7c03cda
sleeg00 <slee000220@gmail.com>
2023-11-08 12:10:34 +0900

    Fix: Comment API

--- src/main/java/com/example/just/Dto/ResponseMyCommentDto.java
+++ src/main/java/com/example/just/Dto/ResponseMyCommentDto.java
@@ -1,25 +1,24 @@
 package com.example.just.Dto;
 
 import com.example.just.Dao.Comment;
-import com.example.just.Dao.Post;
+import com.example.just.Dao.Member;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.List;
 
 @Getter
 @Setter
 public class ResponseMyCommentDto {
     private Long comment_id;
     private String comment_content;
-    private ResponseGetPostDto post;
+    private ResponseGetMemberPostDto post;
     private Date time;
 
-    public ResponseMyCommentDto(Comment comment){
+    public ResponseMyCommentDto(Comment comment, Long member_id, Member member){
         comment_id = comment.getComment_id();
         comment_content = comment.getComment_content();
-        post = new ResponseGetPostDto(comment.getPost());
+        post = new ResponseGetMemberPostDto(comment.getPost(), member_id, member);
         time = comment.getComment_create_time();
     }
 }
