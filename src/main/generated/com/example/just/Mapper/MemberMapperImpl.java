8438da3189b45f1a05da29c07b6798f126a4ac5f
문준호 <munjunho@munjunhoui-MacBookAir.local>
2024-01-10 17:39:14 +0900

    symbol error

--- src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
@@ -10,11 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-<<<<<<< HEAD
-    date = "2024-01-10T17:25:54+0900",
-=======
-    date = "2024-01-10T16:55:17+0900",
->>>>>>> 671c21446276e86892267b1ef25662b74ff59e6d
+    date = "2024-01-10T17:35:33+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
