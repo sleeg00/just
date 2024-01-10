ba47f03d1663c70dcaeb209f96ed7d639cbefdaf
문준호 <munjunho@munjunhoui-MacBookAir.local>
2024-01-10 18:00:44 +0900

    force add

---
+++ src/main/generated/com/example/just/Dao/QMember.java
@@ -0,0 +1,66 @@
+package com.example.just.Dao;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = 2115936519L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final EnumPath<Role> authority = createEnum("authority", Role.class);
+
+    public final NumberPath<Integer> blameCount = createNumber("blameCount", Integer.class);
+
+    public final NumberPath<Integer> blamedCount = createNumber("blamedCount", Integer.class);
+
+    public final ListPath<Comment, QComment> comments = this.<Comment, QComment>createList("comments", Comment.class, QComment.class, PathInits.DIRECT2);
+
+    public final DateTimePath<java.sql.Timestamp> createTime = createDateTime("createTime", java.sql.Timestamp.class);
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final ListPath<Comment, QComment> likedComments = this.<Comment, QComment>createList("likedComments", Comment.class, QComment.class, PathInits.DIRECT2);
+
+    public final ListPath<Post, QPost> likedPosts = this.<Post, QPost>createList("likedPosts", Post.class, QPost.class, PathInits.DIRECT2);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final ListPath<Notification, QNotification> notifications = this.<Notification, QNotification>createList("notifications", Notification.class, QNotification.class, PathInits.DIRECT2);
+
+    public final ListPath<Post, QPost> posts = this.<Post, QPost>createList("posts", Post.class, QPost.class, PathInits.DIRECT2);
+
+    public final StringPath provider = createString("provider");
+
+    public final StringPath provider_id = createString("provider_id");
+
+    public final StringPath refreshToken = createString("refreshToken");
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/example/just/Dao/QPost.java
@@ -0,0 +1,73 @@
+package com.example.just.Dao;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = -1316133299L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QPost post = new QPost("post");
+
+    public final NumberPath<Integer> blamedCount = createNumber("blamedCount", Integer.class);
+
+    public final ListPath<Comment, QComment> comments = this.<Comment, QComment>createList("comments", Comment.class, QComment.class, PathInits.DIRECT2);
+
+    public final StringPath emoticon = createString("emoticon");
+
+    public final ListPath<Member, QMember> likedMembers = this.<Member, QMember>createList("likedMembers", Member.class, QMember.class, PathInits.DIRECT2);
+
+    public final QMember member;
+
+    public final StringPath post_category = createString("post_category");
+
+    public final DateTimePath<java.util.Date> post_create_time = createDateTime("post_create_time", java.util.Date.class);
+
+    public final NumberPath<Long> post_id = createNumber("post_id", Long.class);
+
+    public final NumberPath<Long> post_like = createNumber("post_like", Long.class);
+
+    public final NumberPath<Long> post_picture = createNumber("post_picture", Long.class);
+
+    public final StringPath post_tag = createString("post_tag");
+
+    public final ListPath<String, StringPath> postContent = this.<String, StringPath>createList("postContent", String.class, StringPath.class, PathInits.DIRECT2);
+
+    public final BooleanPath secret = createBoolean("secret");
+
+    public QPost(String variable) {
+        this(Post.class, forVariable(variable), INITS);
+    }
+
+    public QPost(Path<? extends Post> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QPost(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QPost(PathMetadata metadata, PathInits inits) {
+        this(Post.class, metadata, inits);
+    }
+
+    public QPost(Class<? extends Post> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new QMember(forProperty("member")) : null;
+    }
+
+}
+
