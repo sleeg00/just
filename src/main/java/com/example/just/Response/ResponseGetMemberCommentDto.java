4ac7ba29323226d53ed45c406143fded3d39293b
문준호 <munjunho@munjunhoui-MacBookAir.local>
2024-02-23 19:15:42 +0900

    response add

---
+++ src/main/java/com/example/just/Response/ResponseGetMemberCommentDto.java
@@ -0,0 +1,15 @@
+package com.example.just.Response;
+
+import java.util.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseGetMemberCommentDto {
+    private String comment_content;
+    private Long post_id;
+    private Date time;
+    private List<String> post_content;
+}
