a0bc4bcbdd6e13367cd3a33795bd6fa583679293
문준호 <munjunho@munjunhoui-MacBookAir.local>
2024-02-27 16:51:19 +0900

    search paging

--- src/main/java/com/example/just/Repository/PostContentESRespository.java
+++ src/main/java/com/example/just/Repository/PostContentESRespository.java
@@ -1,6 +1,7 @@
 package com.example.just.Repository;
 import com.example.just.Document.PostDocument;
 import org.elasticsearch.search.SearchHits;
+import org.h2.mvstore.Page;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.repository.CrudRepository;
@@ -12,5 +13,6 @@ import java.util.List;
 public interface PostContentESRespository extends ElasticsearchRepository<PostDocument,Long>,
         CrudRepository<PostDocument,Long> {
     List<PostDocument> findByPostContent_ContentContains(String text);
+
 
 }
