5230dd4a45c6b4cd02977101bd3f1725b1b493b4
Ahn-JuHwan <ajuh1103@naver.com>
2024-03-17 14:11:57 +0900

    adminController

--- src/main/generated/com/example/just/Mapper/PostMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/PostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-03-14T12:16:36+0900",
+    date = "2024-03-17T14:02:15+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 11.0.21 (Oracle Corporation)"
 )
 @Component
