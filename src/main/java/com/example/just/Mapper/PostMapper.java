0df7212c03c6272d86237c155ada86be0499fdb2
sleeg00 <slee000220@gmail.com>
2024-03-17 15:13:44 +0900

    gitignore

--- src/main/java/com/example/just/Mapper/PostMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.just.Mapper;
-
-import com.example.just.Dao.Post;
-import com.example.just.Dto.PutPostDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface PostMapper extends EntityMapper<PutPostDto, Post>{
-}
--- src/main/java/com/example/just/Repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.just.Repository;
-
-import com.example.just.Dao.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
