6bbc5148b5d613898971481e71f33cc682df2d68
sleeg00 <slee000220@gmail.com>
2024-07-12 17:29:39 +0900

    feat : Docker gitActions Test

--- src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/MemberMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-03-17T15:34:44+0900",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.11 (AdoptOpenJDK)"
+    date = "2024-07-12T15:43:39+0900",
+    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component
 public class MemberMapperImpl implements MemberMapper {
