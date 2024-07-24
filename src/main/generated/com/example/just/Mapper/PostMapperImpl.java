a4662e913a3304adec8d8eca138820793595fa75
sleeg00 <slee000220@gmail.com>
2024-07-24 18:02:45 +0900

    test : logstash 서버 연동 테스트

--- src/main/generated/com/example/just/Mapper/PostMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/PostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-23T16:27:35+0900",
+    date = "2024-07-24T18:01:13+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component
