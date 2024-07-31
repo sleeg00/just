69a4ad94b6f78160af68cc7cc028659db3ab8163
sleeg00 <slee000220@gmail.com>
2024-07-31 15:32:05 +0900

    fix : log 수집 level 수정

--- src/main/generated/com/example/just/Mapper/PostMapperImpl.java
+++ src/main/generated/com/example/just/Mapper/PostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-26T17:35:53+0900",
+    date = "2024-07-31T15:26:56+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.2.jar, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component
