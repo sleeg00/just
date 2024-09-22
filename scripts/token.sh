05e1a455635f75a65fc259e101235ba538f269ce
sleeg00 <slee000220@gmail.com>
2024-09-22 14:47:14 +0900

    feat : token Shell Script 추가

---
+++ scripts/token.sh
@@ -0,0 +1,12 @@
+# MySQL 접속 정보는 환경 변수로 전달받음
+MYSQL_USER=${MYSQL_USER}
+MYSQL_PASSWORD=${MYSQL_PASSWORD}
+MYSQL_DATABASE=just
+
+# MySQL 명령어 실행
+mysql  -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF
+
+INSERT INTO RefreshToken (email, refresh_token)
+SELECT email, refresh_token
+FROM Member;
+EOF
