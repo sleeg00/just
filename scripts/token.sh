9f92a690068633026cd3d55d04b895f8c9059db0
sleeg00 <slee000220@gmail.com>
2024-09-23 14:59:45 +0900

    fix : token.sh

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # MySQL 접속 정보는 환경 변수로 전달받음
 MYSQL_USER=${MYSQL_USER}
@@ -7,7 +6,6 @@ MYSQL_DATABASE=just
 
 # MySQL 명령어 실행
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "
-DELETE FROM Refresh_token;
 INSERT INTO Refresh_token (email, refresh_token)
 SELECT email, refresh_token FROM Member;
 "
