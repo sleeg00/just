e374686b060163fb3067d444983f379bdeab85c8
sleeg00 <slee000220@gmail.com>
2025-02-25 17:49:07 +0900

    feat: Indexing & N+1 해결(수정 version)

--- src/main/java/com/example/just/Repository/PostRepository.java
+++ src/main/java/com/example/just/Repository/PostRepository.java
@@ -1,6 +1,11 @@
 package com.example.just.Repository;
 
 import com.example.just.Dao.Post;
+import com.example.just.Dao.QHashTag;
+import com.example.just.Dao.QHashTagMap;
+import com.example.just.Dao.QPost;
+import com.example.just.Dao.QPostContent;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.Entity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,10 +16,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+
     List<Post> findByBlamedCountGreaterThanEqualOrderByBlamedCountDesc(int blamed_count);
-
-
     @EntityGraph(attributePaths = {"comments", "member"}, type = EntityGraph.EntityGraphType.LOAD)
     List<Post> findByMemberId(Long member_id);
 
